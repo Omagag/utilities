d41178e375db45b31097d42ca21ff68143285b94
Omar <omar.garcia2@bbva.com>
2018-02-28 18:02:55 -0600

    ServiceResponse Message Manejo de nuevo Error.

--- src/main/java/com/bbva/intranet/utilities/web/ws/utilities/ServiceResponseMessageUtility.java
+++ src/main/java/com/bbva/intranet/utilities/web/ws/utilities/ServiceResponseMessageUtility.java
@@ -24,19 +24,24 @@ public class ServiceResponseMessageUtility {
                 serviceResponse.setCode("00");
                 serviceResponse.setMessage("Successful");
                 break;
-            case EMPTY_REQUEST_BODY:
-                serviceResponse.setCode("02");
-                serviceResponse.setMessage("The request body is empty.");
-                break;
             case GENERIC_ERROR:
                 serviceResponse.setCode("-01");
                 serviceResponse.setMessage("An error has occurred, check logs for more information");
                 serviceResponse.setHttpCode(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
                 serviceResponse.setHttpMessage("Service unavailable.");
                 break;
-
-            // DataBase Messages
-            case DATA_DABE_ERROR:
+            case EMPTY_REQUEST_BODY:
+                serviceResponse.setCode("-02");
+                serviceResponse.setMessage("The request body is empty.");
+                serviceResponse.setHttpCode(HttpServletResponse.SC_OK);
+                break;
+            case JSON_SYNTAX_ERROR:
+                serviceResponse.setCode("-03");
+                serviceResponse.setMessage("JSON Syntax error.");
+                serviceResponse.setHttpCode(HttpServletResponse.SC_OK);
+                break;
+                // DataBase Messages
+            case DATA_BASE_ERROR:
                 serviceResponse.setCode("-10");
                 serviceResponse.setMessage("DataBase error, check logs for more information.");
                 serviceResponse.setHttpCode(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
