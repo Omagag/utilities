127698468bf48ad68f7b2993ded4828cc576b67d
elizabeth.lopez.tapia.contractor <elizabeth.lopez.tapia.contractor@bbva.com>
2019-07-17 18:40:48 -0500

    se agrego un switch para el manejo del mensaje de errores

--- src/main/java/com/bbva/intranet/utilities/web/ws/GenericWsPlus.java
+++ src/main/java/com/bbva/intranet/utilities/web/ws/GenericWsPlus.java
@@ -11,13 +11,23 @@ public class GenericWsPlus extends GenericWs {
     public Response generateResponse(String prefix, CodeError codeError, Exception e, int httpStatusCode,
                                      MessageSource messageSource, Locale locale) {
         Response response;
-
         String code = String.format("%s-%s", prefix, codeError.getCode());
-        String message = String.format("code.error.%s.%s", prefix, codeError.getCode());
+        //String code = String.format("%s",codeError.getCode());
+        String message;
+        switch (codeError) {
+            case GENERIC:
+            case ENOUGH_DATA:
+            case DATABASE:
+                message = String.format("code.error.%s", codeError.getCode());
+                break;
+            default:
+                message = String.format("code.error.%s.%s", prefix, codeError.getCode());
+                break;
+        }
+
         response = generateResponse(code,
                 messageSource.getMessage(message, null, locale),
                 e, httpStatusCode);
-
         return response;
     }
 
--- src/main/java/com/bbva/intranet/utilities/web/ws/enums/CodeError.java
+++ src/main/java/com/bbva/intranet/utilities/web/ws/enums/CodeError.java
@@ -2,7 +2,8 @@ package com.bbva.intranet.utilities.web.ws.enums;
 
 public enum CodeError{
     GENERIC("200"),
-    DATABASE("300");
+    DATABASE("300"),
+    ENOUGH_DATA("101");
 
 
     private String code;
