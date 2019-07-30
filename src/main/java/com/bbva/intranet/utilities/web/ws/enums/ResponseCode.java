0fd0593afc195124bfdc37a682c8e74341411cf1
Omar <omar.garcia2@bbva.com>
2019-07-30 18:29:04 -0500

    Improving Response Codes management.

--- src/main/java/com/bbva/intranet/utilities/web/ws/enums/ResponseCode.java
+++ src/main/java/com/bbva/intranet/utilities/web/ws/enums/ResponseCode.java
@@ -1,14 +1,15 @@
 package com.bbva.intranet.utilities.web.ws.enums;
 
-public enum CodeError{
+public enum ResponseCode {
+
+    SUCCESSFUL("100"),
     GENERIC("200"),
     DATABASE("300"),
     ENOUGH_DATA("101");
 
-
     private String code;
 
-    CodeError(String code) {
+    ResponseCode(String code) {
         this.code = code;
     }
 
