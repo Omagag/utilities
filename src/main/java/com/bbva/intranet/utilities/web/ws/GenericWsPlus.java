53de05c4b852108eea0cd53ce848a69bd040a524
enriquemore587 <jose.vergara2104@hotmail.com>
2019-10-15 17:48:11 -0500

    first commit

--- src/main/java/com/bbva/intranet/utilities/web/ws/GenericWsPlus.java
+++ src/main/java/com/bbva/intranet/utilities/web/ws/GenericWsPlus.java
@@ -5,6 +5,7 @@ import com.google.api.client.http.HttpStatusCodes;
 import com.google.gson.GsonBuilder;
 import org.springframework.context.MessageSource;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.Response;
 import java.util.Locale;
 import java.util.logging.Level;
@@ -21,6 +22,9 @@ public class GenericWsPlus extends GenericWs {
         switch (responseCode) {
             case SUCCESSFUL:
                 message = "code.successful";
+                break;
+            case UNAUTHORIZED:
+                message = "code.unauthorized";
                 break;
             case GENERIC:
             case ENOUGH_DATA:
@@ -59,38 +63,29 @@ public class GenericWsPlus extends GenericWs {
         return generateResponse(prefix, responseCode, obj, null, httpStatusCode, messageSource, locale);
     }
 
-//    protected Response generateResponse(String prefix, ResponseCode responseCode, Exception e, int httpStatusCode,
-//                                        MessageSource messageSource, Locale locale) {
-//        Response response;
-//        String code = String.format("%s-%s", prefix, responseCode.getCode());
-//        //String code = String.format("%s",responseCode.getCode());
-//
-//        String message;
-//        switch (responseCode) {
-//            case GENERIC:
-//            case ENOUGH_DATA:
-//            case DATABASE:
-//                message = String.format("code.error.%s", responseCode.getCode());
-//                break;
-//            default:
-//                message = String.format("code.error.%s.%s", prefix, responseCode.getCode());
-//                break;
-//        }
-//
-//        response = generateResponse(code,
-//                messageSource.getMessage(message, null, locale),
-//                e, httpStatusCode);
-//        return response;
-//    }
+    protected Response validateRequestor(HttpServletRequest request, String wsKey, MessageSource messageSource, Locale locale) {
+        Response response = null;
+        String requestor = request.getHeader("requestor");
+        if (requestor == null || requestor.isEmpty()) {
+            response = generateResponse(wsKey, ResponseCode.UNAUTHORIZED, null,
+                    HttpStatusCodes.STATUS_CODE_UNAUTHORIZED, messageSource, locale);
+        }
+        return response;
+    }
 
+    @Deprecated
     protected Response validateRequestor(String requestor, String wsKey, MessageSource messageSource, Locale locale) {
         Response response = null;
 
         if (requestor == null || requestor.isEmpty()) {
-            response = generateResponse(wsKey, ResponseCode.ENOUGH_DATA, null,
+            response = generateResponse(wsKey, ResponseCode.SUCCESSFUL, null,
                     HttpStatusCodes.STATUS_CODE_BAD_REQUEST, messageSource, locale);
         }
         return response;
+    }
+
+    protected String getRequestor(HttpServletRequest request) {
+        return request.getHeader("requestor");
     }
 
 }
